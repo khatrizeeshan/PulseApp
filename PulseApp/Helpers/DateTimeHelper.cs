2823b2a984921c4368fe70ccbd4a101a402542e6
Zeeshan Khatri <zeeshan.khatri@gmail.com>
2020-11-22 16:24:42 +0500

    Added: Week Days Color

--- PulseApp/Helpers/DateTimeHelper.cs
+++ PulseApp/Helpers/DateTimeHelper.cs
@@ -11,5 +11,26 @@ namespace PulseApp.Helpers
         {
             return new DateTime(2020, month, 1).ToString("MMMM");
         }
+
+        public static bool IsCurrentMonth(int year, int month)
+        {
+            var today = DateTime.Today;
+            return today.Month == month && today.Year == year;
+        }
+
+        public static int[] WeekDays(int year, int month)
+        {
+            return Enumerable.Range(1, DateTime.DaysInMonth(year, month))
+                    .Select(day => new DateTime(year, month, day))
+                    .Where(dt => dt.DayOfWeek == DayOfWeek.Sunday ||
+                                    dt.DayOfWeek == DayOfWeek.Saturday)
+                    .Select(dt => dt.Day)
+                    .ToArray();
+        }
+
+        public static DateTime FirstDay(int year, int month)
+        {
+            return new DateTime(year, month, 1);
+        }
     }
 }
